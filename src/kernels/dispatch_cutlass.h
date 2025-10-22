4ff3fbf8bf73f39bcf7f84bda67934c59cfc3191
ivandobskygithub <84400859+ivandobskygithub@users.noreply.github.com>
2025-10-22 13:38:49 +0100

    Add HIP implementations for GEMM kernels

--- src/kernels/dispatch_cutlass.h
+++ src/kernels/dispatch_cutlass.h
@@ -3,6 +3,28 @@
 #include "common.h"
 #include "Tensor.h"
 
+#if defined(NUNCHAKU_USE_HIP)
+
+#include <hip/hip_fp16.h>
+#if defined(ENABLE_BF16)
+#include <hip/hip_bfloat16.h>
+#endif
+
+template<typename F>
+inline void dispatchF16(Tensor::ScalarType type, F &&func) {
+    if (type == Tensor::FP16) {
+        func.template operator()<half>();
+#if defined(ENABLE_BF16)
+    } else if (type == Tensor::BF16) {
+        func.template operator()<__nv_bfloat16>();
+#endif
+    } else {
+        assert(false);
+    }
+}
+
+#else
+
 #include <cutlass/cutlass.h>
 #include <cutlass/half.h>
 #include <cutlass/bfloat16.h>
@@ -17,3 +39,5 @@ inline void dispatchF16(Tensor::ScalarType type, F &&func) {
         assert(false);
     }
 }
+
+#endif
